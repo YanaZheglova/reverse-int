6beafe95cbd59c6486f4e115a8b64aba04cd7fda
Yana <you@example.com>
2023-01-21 23:41:35 +0300

    add:reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+       n = -1 * n;
+    } else {
+         n = n * 1;
+    }
+    let x = n.toString().split("").reverse().join("");
+    
+    console.log(x);
+  return  x;
 }
